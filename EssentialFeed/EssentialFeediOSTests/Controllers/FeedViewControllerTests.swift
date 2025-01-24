2f963d662adcb265b618fd09420aca904aa4dcec
Luis Segovia <luissegovia@outlook.com>
2025-01-23 22:35:13 -0600

    Extract FeedImageCell test helpers to a separate file

--- EssentialFeed/EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
@@ -336,33 +336,3 @@ final class FeedViewControllerTests: XCTestCase {
     }
 
 }
-
-private extension FeedImageCell {
-    func simulateRetryAction() {
-        feedImageRetryButton.simulateTap()
-    }
-
-    var isShowingLocation: Bool {
-        return !locationContainer.isHidden
-    }
-
-    var isShowingImageLoadingIndicator: Bool {
-        return feedImageContainer.isShimmering
-    }
-
-    var renderedImage: Data? {
-        return feedImageView.image?.pngData()
-    }
-
-    var isShowingRetryAction: Bool {
-        return !feedImageRetryButton.isHidden
-    }
-
-    var locationText: String? {
-        return locationLabel.text
-    }
-
-    var descriptionText: String? {
-        return descriptionLabel.text
-    }
-}
---
+++ EssentialFeed/EssentialFeediOSTests/Controllers/Helpers/FeedImageCell+TestHelpers.swift
@@ -0,0 +1,39 @@
+//
+//  FeedImageCell+TestHelpers.swift
+//  EssentialFeed
+//
+//  Created by Luis Segovia on 23/01/25.
+//
+
+import UIKit
+import EssentialFeediOS
+
+extension FeedImageCell {
+    func simulateRetryAction() {
+        feedImageRetryButton.simulateTap()
+    }
+
+    var isShowingLocation: Bool {
+        return !locationContainer.isHidden
+    }
+
+    var isShowingImageLoadingIndicator: Bool {
+        return feedImageContainer.isShimmering
+    }
+
+    var renderedImage: Data? {
+        return feedImageView.image?.pngData()
+    }
+
+    var isShowingRetryAction: Bool {
+        return !feedImageRetryButton.isHidden
+    }
+
+    var locationText: String? {
+        return locationLabel.text
+    }
+
+    var descriptionText: String? {
+        return descriptionLabel.text
+    }
+}
