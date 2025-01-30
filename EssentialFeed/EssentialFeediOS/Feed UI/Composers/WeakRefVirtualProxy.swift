bb17df677b08f54ce38dc60abb81b269f416a911
Luis Segovia <luissegovia@outlook.com>
2025-01-30 15:49:37 -0600

    Move `WeakRefVirtualProxy` to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/WeakRefVirtualProxy.swift
@@ -0,0 +1,28 @@
+//
+//  WeakRefVirtualProxy.swift
+//  EssentialFeed
+//
+//  Created by Luis Segovia on 30/01/25.
+//
+
+import UIKit
+
+final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
+    func display(_ model: FeedImageViewModel<UIImage>) {
+        object?.display(model)
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
