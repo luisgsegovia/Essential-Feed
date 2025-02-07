6636df40e4f12d2938c8b76c6efaf9cba5154289
Luis Segovia <luissegovia@outlook.com>
2025-02-06 21:30:12 -0600

    Add UI Test target for running high-level Acceptance Tests

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,15 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialApp
+//
+//  Created by Luis Segovia on 06/02/25.
+//
+
+
+import XCTest
+
+class EssentialAppUIAcceptanceTests: XCTestCase {
+
+
+
+}
