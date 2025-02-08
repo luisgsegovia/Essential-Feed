51b68f4d204a200ae59f575af244bda6db083f53
Luis Segovia <luissegovia@outlook.com>
2025-02-08 12:51:33 -0600

    Configures feed navigation as window root view controller

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -24,6 +24,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
 
+        configureWindow()
+    }
+
+    func configureWindow() {
         let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
 
         let remoteClient = makeRemoteClient()
@@ -35,16 +39,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let localImageLoader = LocalFeedImageDataLoader(store: localStore)
 
         window?.rootViewController = FeedUIComposer.feedComposedWith(
-        feedLoader: FeedLoaderWithFallbackComposite(
-            primary: FeedLoaderCacheDecorator(
-                decoratee: remoteFeedLoader,
-                cache: localFeedLoader),
-            fallback: localFeedLoader),
-        imageLoader: FeedImageDataLoaderWithFallbackComposite(
-            primary: localImageLoader,
-            fallback: FeedImageDataLoaderCacheDecorator(
-                decoratee: remoteImageLoader,
-                cache: localImageLoader)))
+            feedLoader: FeedLoaderWithFallbackComposite(
+                primary: FeedLoaderCacheDecorator(
+                    decoratee: remoteFeedLoader,
+                    cache: localFeedLoader),
+                fallback: localFeedLoader),
+            imageLoader: FeedImageDataLoaderWithFallbackComposite(
+                primary: localImageLoader,
+                fallback: FeedImageDataLoaderCacheDecorator(
+                    decoratee: remoteImageLoader,
+                    cache: localImageLoader)))
     }
 
     func makeRemoteClient() -> HTTPClient {
